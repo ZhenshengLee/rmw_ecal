53bd4ca07ad6e4c3dd3204754ae09e5d4b283255
lizhensheng <lzs_1993@qq.com>
2026-07-09 19:20:45 +0800

    fix: remove typesupport message name with proto: prefix

--- rmw_ecal_proto_cpp/src/typesupport/proto_message_typesupport.hpp
+++ rmw_ecal_proto_cpp/src/typesupport/proto_message_typesupport.hpp
@@ -49,7 +49,7 @@ namespace eCAL
 
       virtual const std::string GetMessageName() const override
       {
-        return "proto:" + GetMessageNamespace() + "/" + GetMessageSimpleName();
+        return GetMessageNamespace() + "/" + GetMessageSimpleName();
       }
 
       virtual size_t GetTypeSize() const override
